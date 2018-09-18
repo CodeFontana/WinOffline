01dc591f11faa4765d729ae2f560fef488a49277
Brian Fontana <bjfontana@outlook.com>
2018-09-18 15:26:34 -0400

    Additional debugging added

--- WinOffline/Utility.vb
+++ WinOffline/Utility.vb
@@ -1488,26 +1488,37 @@ Partial Public Class WinOffline
         End Function
 
         Public Shared Function IsProcessRunning(ByVal ProcessFriendlyName As String) As Boolean
-
-            ' Loop running processes
             For Each RunningProcess As Process In Process.GetProcesses()
-
-                ' Match the process name
                 If RunningProcess.ProcessName.ToLower.Equals(ProcessFriendlyName.ToLower) Then
-
-                    ' Debug
-                    Logger.WriteDebug(Logger.LastCallStack, "IsProcessRunning() found: " + RunningProcess.Id.ToString + "/" + RunningProcess.ProcessName)
-
-                    ' Match found
+                    Dim ProcessWMI As ManagementObject = Nothing
+                    Dim CurrentID As Integer = Nothing
+                    Dim ParentID As Integer = Nothing
+                    Dim ParentName As String = Nothing
+                    Dim WMIQuery As ManagementObjectSearcher
+                    Dim CommandLine As String = Nothing
+                    WMIQuery = New ManagementObjectSearcher("SELECT * FROM Win32_Process WHERE ProcessId='" + RunningProcess.Id.ToString + "'")
+                    For Each WMIProcess As ManagementObject In WMIQuery.Get()
+                        CommandLine = WMIProcess("CommandLine").ToString
+                    Next
+                    Logger.WriteDebug(Logger.LastCallStack, "IsProcessRunning() found: " + RunningProcess.Id.ToString + "/" + RunningProcess.ProcessName + " " + CommandLine)
+                    Try
+                        CurrentID = RunningProcess.Id
+                        While True
+                            ProcessWMI = New ManagementObject("Win32_Process.Handle='" & CurrentID & "'")
+                            ParentID = ProcessWMI("ParentProcessID")
+                            ParentName = Process.GetProcessById(ParentID).ProcessName.ToString
+                            Logger.WriteDebug(Logger.LastCallStack, "IsProcessRunning() parent: " + ParentID.ToString + "/" + ParentName)
+                            If Globals.ParentProcessName Is Nothing Then Globals.ParentProcessName = ParentName.ToLower
+                            Globals.ParentProcessTree.Add(ParentName.ToLower)
+                            CurrentID = ParentID
+                        End While
+                    Catch ex As Exception
+                        If Globals.ParentProcessName Is Nothing Then Globals.ParentProcessName = "noparent"
+                    End Try
                     Return True
-
                 End If
-
             Next
-
-            ' Process not found
             Return False
-
         End Function
 
         Public Shared Function IsProcessRunning(ByVal ProcessShortName As String, ByVal ContainsCommandLine As String) As Boolean
