daf5a1a39623c1d2de3aac86d154a687f6cb0fbb
CodeFontana <41308769+CodeFontana@users.noreply.github.com>
2018-09-20 18:11:14 -0400

    Code cleanup.

--- WinOffline/WinOfflineUI-Sql-EngineGrid.vb
+++ WinOffline/WinOfflineUI-Sql-EngineGrid.vb
@@ -9,12 +9,10 @@ Partial Public Class WinOfflineUI
 
     Private Sub InitSqlEngineGrid()
 
-        ' Disable buttons
         Delegate_Sub_Enable_Red_Button(btnSqlDisconnectEngineGrid, False)
         Delegate_Sub_Enable_Blue_Button(btnSqlRefreshEngineGrid, False)
         Delegate_Sub_Enable_Blue_Button(btnSqlExportEngineGrid, False)
 
-        ' Set engine grid properties
         dgvEngineGrid.AllowUserToAddRows = False
         dgvEngineGrid.AllowUserToDeleteRows = False
         dgvEngineGrid.AllowUserToResizeRows = False
@@ -42,20 +40,13 @@ Partial Public Class WinOfflineUI
 
     Private Sub EngineGridWorker(ByVal ConnectionString As String)
 
-        ' Local variables
         Dim DbConnection As SqlConnection = New SqlConnection(ConnectionString)
         Dim CallStack As String = "EngineGridWorker --> "
 
-        ' Encapsulate grid worker
         Try
-
-            ' Open sql connection
             DbConnection.Open()
-
-            ' Write debug
             Delegate_Sub_Append_Text(rtbDebug, CallStack + "Connected successful: " + SqlServer)
 
-            ' Reveal progress bar
             grpSqlEngineGrid.Invoke(Sub() grpSqlEngineGrid.Height = grpSqlEngineGrid.Height - prgTableSpaceGrid.Height - 4)
 
             ' Query engine grid
@@ -69,125 +60,71 @@ Partial Public Class WinOfflineUI
                                           prgEngineGrid,
                                           grpSqlEngineGrid)
 
-            ' Hide progress bar
             grpSqlEngineGrid.Invoke(Sub() grpSqlEngineGrid.Height = pnlSqlEngineGrid.Height - pnlSqlEngineGridButtons.Height - 3)
 
         Catch ex As Exception
-
-            ' Write debug
             Delegate_Sub_Append_Text(rtbDebug, CallStack + "Exception:" + Environment.NewLine + ex.Message)
             Delegate_Sub_Append_Text(rtbDebug, CallStack + "Stack trace: " + Environment.NewLine + ex.StackTrace)
-
         Finally
-
-            ' Check if database connection is open
             If Not DbConnection.State = ConnectionState.Closed Then
-
-                ' Close the database connection
                 DbConnection.Close()
-
-                ' Write debug
                 Delegate_Sub_Append_Text(rtbDebug, CallStack + "Database connection closed.")
-
             End If
-
-            ' Enable buttons
             Delegate_Sub_Enable_Yellow_Button(btnSqlRefreshEngineGrid, True)
             Delegate_Sub_Enable_Tan_Button(btnSqlExportEngineGrid, True)
-
         End Try
 
     End Sub
 
     Private Sub btnSqlConnectEngineGrid_Click(sender As Object, e As EventArgs) Handles btnSqlConnectEngineGrid.Click
-
-        ' Perform SQL connection
         SqlConnect()
-
     End Sub
 
     Private Sub btnSqlDisconnectEngineGrid_Click(sender As Object, e As EventArgs) Handles btnSqlDisconnectEngineGrid.Click
-
-        ' Perform disconnect method
         SqlDisconnect()
-
     End Sub
 
     Private Sub btnSqlRefreshEngineGrid_Click(sender As Object, e As EventArgs) Handles btnSqlRefreshEngineGrid.Click
-
-        ' Disable buttons
         Delegate_Sub_Enable_Yellow_Button(btnSqlRefreshEngineGrid, False)
         Delegate_Sub_Enable_Tan_Button(btnSqlExportEngineGrid, False)
-
-        ' Restart thread
         EngineGridThread = New Thread(Sub() EngineGridWorker(ConnectionString))
         EngineGridThread.Start()
-
     End Sub
 
     Private Sub btnSqlExportEngineGrid_Click(sender As Object, e As EventArgs) Handles btnSqlExportEngineGrid.Click
 
-        ' Local variables
         Dim saveFileDialog1 As New SaveFileDialog()
         Dim StateStreamWriter As System.IO.StreamWriter
 
-        ' Set dialog properties
         saveFileDialog1.Filter = "CSV (Comma delimited)|*.csv"
         saveFileDialog1.Title = "Save a CSV File"
 
-        ' Launch dialog and check result
         If saveFileDialog1.ShowDialog() = DialogResult.Cancel Then Return
 
-        ' Encapsulate file operation
         Try
-
-            ' Open output stream
             StateStreamWriter = New System.IO.StreamWriter(saveFileDialog1.FileName, False)
 
-            ' Iterate datagrid column headers
             For Each dgvColumn As DataGridViewColumn In dgvEngineGrid.Columns
-
-                ' Write values
                 StateStreamWriter.Write(dgvColumn.HeaderText + ",")
-
             Next
-
-            ' Write newline
             StateStreamWriter.Write(Environment.NewLine)
 
-            ' Iterate datagrid rows
             For Each dgvRecord As DataGridViewRow In dgvEngineGrid.Rows
-
-                ' Iterate cells
                 For Each CellItem As DataGridViewCell In dgvRecord.Cells
-
-                    ' Write values
                     StateStreamWriter.Write(CellItem.Value.ToString + ",")
-
                 Next
-
-                ' Write newline
                 StateStreamWriter.Write(Environment.NewLine)
-
             Next
 
-            ' Close output stream
             StateStreamWriter.Close()
-
         Catch ex As Exception
-
-            ' Push user alert
             AlertBox.CreateUserAlert("Export failed." + Environment.NewLine + Environment.NewLine + "Exception: " + ex.Message)
-
         End Try
 
     End Sub
 
     Private Sub btnSqlExitEngineGrid_Click(sender As Object, e As EventArgs) Handles btnSqlExitEngineGrid.Click
-
-        ' Close the dialog
         Close()
-
     End Sub
 
 End Class
