f39b3bdc9bdefc751176c47c2eb9ac4ab19d7608
CodeFontana <41308769+CodeFontana@users.noreply.github.com>
2018-09-20 15:40:20 -0400

    Code cleanup.

--- WinOffline/ComstoreAPI.vb
+++ WinOffline/ComstoreAPI.vb
@@ -9,17 +9,22 @@
             Dim RunningProcess As Process
             Dim ProcessStartInfo As ProcessStartInfo
             Dim StandardOutput As String
+
             ExecutionString = Globals.DSMFolder + "bin\" + "ccnfcmda.exe"
             ArgumentString = "-cmd getparametervalue -ps " + ParameterSection + " -pn " + ParameterName
+
             ProcessStartInfo = New ProcessStartInfo(ExecutionString, ArgumentString)
             ProcessStartInfo.WorkingDirectory = Globals.DSMFolder + "bin"
             ProcessStartInfo.UseShellExecute = False
             ProcessStartInfo.RedirectStandardOutput = True
             ProcessStartInfo.CreateNoWindow = True
+
             RunningProcess = Process.Start(ProcessStartInfo)
             RunningProcess.WaitForExit()
+
             StandardOutput = RunningProcess.StandardOutput.ReadToEnd
             RunningProcess.Close()
+
             Return StandardOutput.Replace(vbCr, "").Replace(vbLf, "") ' Return -- remove carriage return (with full safety)
 
         End Function
@@ -31,17 +36,22 @@
             Dim RunningProcess As Process
             Dim ProcessStartInfo As ProcessStartInfo
             Dim StandardOutput As String
+
             ExecutionString = Globals.DSMFolder + "bin\" + "ccnfcmda.exe"
             ArgumentString = "-cmd setparametervalue -ps " + ParameterSection + " -pn " + ParameterName + " -v """ + NewValue + """"
+
             ProcessStartInfo = New ProcessStartInfo(ExecutionString, ArgumentString)
             ProcessStartInfo.WorkingDirectory = Globals.DSMFolder + "bin"
             ProcessStartInfo.UseShellExecute = False
             ProcessStartInfo.RedirectStandardOutput = True
             ProcessStartInfo.CreateNoWindow = True
+
             RunningProcess = Process.Start(ProcessStartInfo)
             RunningProcess.WaitForExit()
+
             StandardOutput = RunningProcess.StandardOutput.ReadToEnd
             RunningProcess.Close()
+
             Return StandardOutput.Replace(vbCr, "").Replace(vbLf, "") ' Return -- remove carriage return (with full safety)
 
         End Function
@@ -53,17 +63,22 @@
             Dim RunningProcess As Process
             Dim ProcessStartInfo As ProcessStartInfo
             Dim StandardOutput As String
+
             ExecutionString = Globals.DSMFolder + "bin\" + "ccnfcmda.exe"
             ArgumentString = "-cmd deleteparameter -ps " + ParameterSection + " -pn " + ParameterName
+
             ProcessStartInfo = New ProcessStartInfo(ExecutionString, ArgumentString)
             ProcessStartInfo.WorkingDirectory = Globals.DSMFolder + "bin"
             ProcessStartInfo.UseShellExecute = False
             ProcessStartInfo.RedirectStandardOutput = True
             ProcessStartInfo.CreateNoWindow = True
+
             RunningProcess = Process.Start(ProcessStartInfo)
             RunningProcess.WaitForExit()
+
             StandardOutput = RunningProcess.StandardOutput.ReadToEnd
             RunningProcess.Close()
+
             Return StandardOutput.Replace(vbCr, "").Replace(vbLf, "") ' Return -- remove carriage return (with full safety)
 
         End Function
@@ -75,17 +90,22 @@
             Dim RunningProcess As Process
             Dim ProcessStartInfo As ProcessStartInfo
             Dim StandardOutput As String
+
             ExecutionString = Globals.DSMFolder + "bin\" + "ccnfcmda.exe"
             ArgumentString = "-cmd deleteparamsection -ps " + ParameterSection
+
             ProcessStartInfo = New ProcessStartInfo(ExecutionString, ArgumentString)
             ProcessStartInfo.WorkingDirectory = Globals.DSMFolder + "bin"
             ProcessStartInfo.UseShellExecute = False
             ProcessStartInfo.RedirectStandardOutput = True
             ProcessStartInfo.CreateNoWindow = True
+
             RunningProcess = Process.Start(ProcessStartInfo)
             RunningProcess.WaitForExit()
+
             StandardOutput = RunningProcess.StandardOutput.ReadToEnd
             RunningProcess.Close()
+
             Return StandardOutput.Replace(vbCr, "").Replace(vbLf, "") ' Return -- remove carriage return (with full safety)
 
         End Function
