1d56ea5fab8dc79d84b2fbcf76d4cef47393f3ec
Brian Fontana <bjfontana@outlook.com>
2018-09-21 12:37:08 -0400

    Code cleanup.

--- WinOffline/WinOfflineUI-Sql-UserGrid.vb
+++ WinOffline/WinOfflineUI-Sql-UserGrid.vb
@@ -9,12 +9,10 @@ Partial Public Class WinOfflineUI
 
     Private Sub InitSqlUserGrid()
 
-        ' Disable buttons
         Delegate_Sub_Enable_Red_Button(btnSqlDisconnectUserGrid, False)
         Delegate_Sub_Enable_Blue_Button(btnSqlRefreshUserGrid, False)
         Delegate_Sub_Enable_Blue_Button(btnSqlExportUserGrid, False)
 
-        ' Set grid properties
         dgvUserGrid.AllowUserToAddRows = False
         dgvUserGrid.AllowUserToDeleteRows = False
         dgvUserGrid.AllowUserToResizeRows = False
@@ -38,7 +36,6 @@ Partial Public Class WinOfflineUI
         dgvUserGrid.ShowEditingIcon = False
         dgvUserGrid.ShowRowErrors = False
 
-        ' Set grid properties
         dgvUserObsolete90.AllowUserToAddRows = False
         dgvUserObsolete90.AllowUserToDeleteRows = False
         dgvUserObsolete90.AllowUserToResizeRows = False
@@ -62,7 +59,6 @@ Partial Public Class WinOfflineUI
         dgvUserObsolete90.ShowEditingIcon = False
         dgvUserObsolete90.ShowRowErrors = False
 
-        ' Set grid properties
         dgvUserObsolete365.AllowUserToAddRows = False
         dgvUserObsolete365.AllowUserToDeleteRows = False
         dgvUserObsolete365.AllowUserToResizeRows = False
@@ -95,20 +91,13 @@ Partial Public Class WinOfflineUI
 
     Private Sub UserGridWorker(ByVal ConnectionString As String)
 
-        ' Local variables
         Dim DbConnection As SqlConnection = New SqlConnection(ConnectionString)
         Dim CallStack As String = "UserGridWorker --> "
 
-        ' Encapsulate grid worker
         Try
-
-            ' Open sql connection
             DbConnection.Open()
-
-            ' Write debug
             Delegate_Sub_Append_Text(rtbDebug, CallStack + "Connected successful: " + SqlServer)
 
-            ' Reveal progress bar
             tabCtrlUserGrid.Invoke(Sub() tabCtrlUserGrid.Height = tabCtrlUserGrid.Height - prgUserGrid.Height - 4)
 
             ' Query user summary
@@ -144,201 +133,105 @@ Partial Public Class WinOfflineUI
                                           prgUserGrid,
                                           tabUserObsolete365)
 
-            ' Hide progress bar
             tabCtrlUserGrid.Invoke(Sub() tabCtrlUserGrid.Height = pnlSqlUserGrid.Height - pnlSqlUserGridButtons.Height - 3)
 
         Catch ex As Exception
-
-            ' Write debug
             Delegate_Sub_Append_Text(rtbDebug, CallStack + "Exception:" + Environment.NewLine + ex.Message)
             Delegate_Sub_Append_Text(rtbDebug, CallStack + "Stack trace: " + Environment.NewLine + ex.StackTrace)
-
         Finally
-
-            ' Check if database connection is open
             If Not DbConnection.State = ConnectionState.Closed Then
-
-                ' Close the database connection
                 DbConnection.Close()
-
-                ' Write debug
                 Delegate_Sub_Append_Text(rtbDebug, CallStack + "Database connection closed.")
-
             End If
-
-            ' Enable buttons
             Delegate_Sub_Enable_Yellow_Button(btnSqlRefreshUserGrid, True)
             Delegate_Sub_Enable_Tan_Button(btnSqlExportUserGrid, True)
-
         End Try
 
     End Sub
 
     Private Sub btnSqlConnectUserGrid_Click(sender As Object, e As EventArgs) Handles btnSqlConnectUserGrid.Click
-
-        ' Perform SQL connection
         SqlConnect()
-
     End Sub
 
     Private Sub btnSqlDisconnectUserGrid_Click(sender As Object, e As EventArgs) Handles btnSqlDisconnectUserGrid.Click
-
-        ' Check if progress bar is visible
         If tabCtrlUserGrid.Height + prgUserGrid.Height + pnlSqlUserGridButtons.Height < pnlSqlUserGrid.Height Then
-
-            ' Hide progress bar
             tabCtrlUserGrid.Invoke(Sub() tabCtrlUserGrid.Height = tabCtrlUserGrid.Height + prgUserGrid.Height)
-
         End If
-
-        ' Perform disconnect method
         SqlDisconnect()
-
     End Sub
 
     Private Sub btnSqlRefreshUserGrid_Click(sender As Object, e As EventArgs) Handles btnSqlRefreshUserGrid.Click
-
-        ' Disable buttons
         Delegate_Sub_Enable_Yellow_Button(btnSqlRefreshUserGrid, False)
         Delegate_Sub_Enable_Tan_Button(btnSqlExportUserGrid, False)
-
-        ' Reset tab text
         If tabUserSummary.Text.Contains("(") Then Delegate_Sub_Set_Text(tabUserSummary, tabUserSummary.Text.Substring(0, tabUserSummary.Text.IndexOf("(") - 1))
         If tabUserObsolete90.Text.Contains("(") Then Delegate_Sub_Set_Text(tabUserObsolete90, tabUserObsolete90.Text.Substring(0, tabUserObsolete90.Text.IndexOf("(") - 1))
         If tabUserObsolete365.Text.Contains("(") Then Delegate_Sub_Set_Text(tabUserObsolete365, tabUserObsolete365.Text.Substring(0, tabUserObsolete365.Text.IndexOf("(") - 1))
-
-        ' Restart thread
         UserGridThread = New Thread(Sub() UserGridWorker(ConnectionString))
         UserGridThread.Start()
-
     End Sub
 
     Private Sub btnSqlExportUserGrid_Click(sender As Object, e As EventArgs) Handles btnSqlExportUserGrid.Click
 
-        ' Local variables
         Dim saveFileDialog1 As New SaveFileDialog()
         Dim StateStreamWriter As System.IO.StreamWriter
 
-        ' Set dialog properties
         saveFileDialog1.Filter = "CSV (Comma delimited)|*.csv"
         saveFileDialog1.Title = "Save a CSV File"
 
-        ' Launch dialog and check result
         If saveFileDialog1.ShowDialog() = DialogResult.Cancel Then Return
 
-        ' Encapsulate file operation
         Try
-
-            ' Open output stream
             StateStreamWriter = New System.IO.StreamWriter(saveFileDialog1.FileName, False)
 
-            ' Check selected tab
             If tabCtrlUserGrid.SelectedTab.Equals(tabUserSummary) Then
-
-                ' Iterate datagrid column headers
                 For Each dgvColumn As DataGridViewColumn In dgvUserGrid.Columns
-
-                    ' Write values
                     StateStreamWriter.Write(dgvColumn.HeaderText + ",")
-
                 Next
-
-                ' Write newline
                 StateStreamWriter.Write(Environment.NewLine)
 
-                ' Iterate datagrid rows
                 For Each dgvRecord As DataGridViewRow In dgvUserGrid.Rows
-
-                    ' Iterate cells
                     For Each CellItem As DataGridViewCell In dgvRecord.Cells
-
-                        ' Write values
                         StateStreamWriter.Write(CellItem.Value.ToString + ",")
-
                     Next
-
-                    ' Write newline
                     StateStreamWriter.Write(Environment.NewLine)
-
                 Next
 
             ElseIf tabCtrlUserGrid.SelectedTab.Equals(tabUserObsolete90) Then
-
-                ' Iterate datagrid column headers
                 For Each dgvColumn As DataGridViewColumn In dgvUserObsolete90.Columns
-
-                    ' Write values
                     StateStreamWriter.Write(dgvColumn.HeaderText + ",")
-
                 Next
-
-                ' Write newline
                 StateStreamWriter.Write(Environment.NewLine)
 
-                ' Iterate datagrid rows
                 For Each dgvRecord As DataGridViewRow In dgvUserObsolete90.Rows
-
-                    ' Iterate cells
                     For Each CellItem As DataGridViewCell In dgvRecord.Cells
-
-                        ' Write values
                         StateStreamWriter.Write(CellItem.Value.ToString + ",")
-
                     Next
-
-                    ' Write newline
                     StateStreamWriter.Write(Environment.NewLine)
-
                 Next
 
             ElseIf tabCtrlUserGrid.SelectedTab.Equals(tabUserObsolete365) Then
-
-                ' Iterate datagrid column headers
                 For Each dgvColumn As DataGridViewColumn In dgvUserObsolete365.Columns
-
-                    ' Write values
                     StateStreamWriter.Write(dgvColumn.HeaderText + ",")
-
                 Next
-
-                ' Write newline
                 StateStreamWriter.Write(Environment.NewLine)
 
-                ' Iterate datagrid rows
                 For Each dgvRecord As DataGridViewRow In dgvUserObsolete365.Rows
-
-                    ' Iterate cells
                     For Each CellItem As DataGridViewCell In dgvRecord.Cells
-
-                        ' Write values
                         StateStreamWriter.Write(CellItem.Value.ToString + ",")
-
                     Next
-
-                    ' Write newline
                     StateStreamWriter.Write(Environment.NewLine)
-
                 Next
-
             End If
 
-            ' Close output stream
             StateStreamWriter.Close()
-
         Catch ex As Exception
-
-            ' Push user alert
             AlertBox.CreateUserAlert("Export failed." + Environment.NewLine + Environment.NewLine + "Exception: " + ex.Message)
-
         End Try
 
     End Sub
 
     Private Sub btnExitSqlUserGrid_Click(sender As Object, e As EventArgs) Handles btnSqlExitUserGrid.Click
-
-        ' Close the dialog
         Close()
-
     End Sub
 
 End Class
