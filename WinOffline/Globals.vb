31ff65d6c926bcb342fe3d086f2bfa720b237c92
Brian Fontana <bjfontana@outlook.com>
2019-01-10 01:11:42 -0500

    Version tick.

--- WinOffline/Globals.vb
+++ WinOffline/Globals.vb
@@ -4,7 +4,7 @@ Public Class Globals
 
     ' WinOffline and System Globals
     Public Shared CommandLineArgs As String() = Nothing                         ' Command line arguments passed to the application.
-    Public Shared AppVersion As String = "2018.01.07"                           ' Version string of the current build.
+    Public Shared AppVersion As String = "2018.01.10"                           ' Version string of the current build.
     Public Shared ProcessName As String = Nothing                               ' Fullpath including filename of the application process.
     Public Shared ProcessShortName As String = Nothing                          ' Filename of the application process.
     Public Shared ProcessFriendlyName As String = Nothing                       ' Friendly name of the application process.
