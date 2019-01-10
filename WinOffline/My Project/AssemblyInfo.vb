4c30c4281b963e633ef64820c7e580bd0b520a00
Brian Fontana <bjfontana@outlook.com>
2019-01-10 01:51:16 -0500

    Release build.

--- WinOffline/My Project/AssemblyInfo.vb
+++ WinOffline/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("2018.01.10")>
-<Assembly: AssemblyFileVersion("2018.01.10")>
+<Assembly: AssemblyVersion("2019.01.10")>
+<Assembly: AssemblyFileVersion("2019.01.10")>
