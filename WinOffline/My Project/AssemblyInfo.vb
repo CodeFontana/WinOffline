1cf4389bf57168db301035e823355cb54885fabd
Brian Fontana <bjfontana@outlook.com>
2019-05-17 11:20:40 -0400

    Version tick.

--- WinOffline/My Project/AssemblyInfo.vb
+++ WinOffline/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("2019.05.08")>
-<Assembly: AssemblyFileVersion("2019.05.08")>
+<Assembly: AssemblyVersion("2019.05.17")>
+<Assembly: AssemblyFileVersion("2019.05.17")>
